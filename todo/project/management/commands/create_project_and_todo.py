9805eee9d2a9abb8576e26f5081b07ba7e0ef263
ekaterina_pchelenko <e.pchelenko@gmail.com>
2022-01-08 19:40:44 +0500

    updated management

--- todo/project/management/commands/create_project_and_todo.py
+++ todo/project/management/commands/create_project_and_todo.py
@@ -6,3 +6,4 @@ from todo.project.models import Project, ToDo
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Project.objects.create(name='project1', repository_link='https://github.com/EkaterinaPchelenko/drf')
+        ToDo.objects.create(project=1, text='test', creator=1)
